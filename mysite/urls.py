1ed1ddc7fe0475036ef470c5a150d3dd2365d017
Jeffi Tapserda <55074292+tapserda@users.noreply.github.com>
2019-12-13 08:36:14 +0700

    add index

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', include('polls.urls')),
     path('polls/', include('polls.urls')),
 ]
